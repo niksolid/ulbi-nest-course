e4ece60298b53f043317d6723b0a381ad502be85
niksolid <semenov.send@gmail.com>
2024-03-19 21:53:56 +0300

    add: creation jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,34 @@
-import { Body, Injectable, Post } from "@nestjs/common";
+import { Body, HttpException, HttpStatus, Injectable, Post } from "@nestjs/common";
 import { CreateUserDto } from "../users/dto/create-user.dto";
 import { UsersService } from "../users/users.service";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcryptjs";
+import { User } from "src/users/users.model";
 
 @Injectable()
 export class AuthService {
 
-  constructor(private userService: UsersService) {
+  constructor(private userService: UsersService,
+              private jwtService: JwtService) {
   }
+
   async login(userDto: CreateUserDto) {
   }
 
   async registration(userDto: CreateUserDto) {
+    const candidate = await this.userService.getUsersByEmail(userDto.email);
+    if (candidate) {
+      throw new HttpException("Пользователь с таким email существует", HttpStatus.BAD_REQUEST);
+    }
+    const hashPassword = await bcrypt.hash(userDto.password, 5);
+    const user = await this.userService.createUser({...userDto, password: hashPassword})
+    return this.generateToken(user)
+  }
+
+  async generateToken(user: User) {
+    const payload = {email: user.email, id: user.id, roles: user.roles}
+    return {
+      token: this.jwtService.sign(payload)
+    }
   }
 }
